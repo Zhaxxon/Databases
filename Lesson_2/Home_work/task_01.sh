6db25eccf7556d5be422b2f8f5a158847c0a754b
Zhaxxon <Zhardemov.Zh@gmail.com>
2019-10-09 19:59:20 +0500

    Done task_01. There was created .my.cnf to enter MySQL without user and password

---
+++ Lesson_2/Home_work/task_01.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Установите СУБД MySQL. Создайте в домашней директории файл .my.cnf, задав в нем логин и пароль, который указывался при установке.
+
+echo '[mysql]' > ~/.my.cnf
+echo 'user=root' >> ~/.my.cnf
+echo 'password=master' >> ~/.my.cnf
