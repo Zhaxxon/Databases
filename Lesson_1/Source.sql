6b0419c3eb55a2df7427dbe5fb541802e9562566
Zhaxxon <Zhardemov.Zh@gmail.com>
2019-10-14 08:07:57 +0500

    Source example code is changed

--- Lesson_1/Source.sql
+++ Lesson_1/Source.sql
@@ -1,60 +1,84 @@
-create database geekbrains;
-use geekbrains;
--- DDL - Data Definaiton language
--- ??????? ??????
-create table courses (
-	id INT auto_increment not null primary key,
-	name VARCHAR(150) unique
+-- Вебинар 1
+-- Быстрый старт
+
+-- Заходим в консольный клиент
+mysql -u root -p
+
+-- Просмотрим существующие БД 
+SHOW DATABASES;
+
+-- Создадим БД и рассмотрим основные команды DDL
+
+-- команды DDL (data definition language)
+-- Создадим базу данных
+CREATE DATABASE geekbrains;
+
+--------------------------------------------------------------------------
+
+USE geekbrains;
+
+-- Таблица курсов
+CREATE TABLE courses (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  name VARCHAR(150) UNIQUE
 );
-/*
-char(10)
-DB*******
-varchar(10)
-DB
-*/
 
-create table students (
-	id INT auto_increment not null primary key,
-	firstname VARCHAR(150) not null,
-	lastname VARCHAR(150) not null,
-	email VARCHAR(150) unique,
-	course_id INT
-)
+-- Таблица студентов
+CREATE TABLE students (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  firstname VARCHAR(100) NOT NULL,
+  lastname VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  course_id INT -- может ли студент взять несколько курсов? (проверка подготовки, интерактив)
+); 
 
-alter table students
-add column birthday date;
+-- Выведем список таблиц
+SHOW TABLES;
 
-alter table students
-drop column date_of_birth
+-- Выведем описание структуры таблиц
+DESCRIBE courses;
+DESCRIBE students;
 
-drop table students;
+-- Меняем структуру таблицы
+ALTER TABLE students ADD COLUMN birthday DATE; 
+ALTER TABLE students RENAME COLUMN birthday TO date_of_birth;
+ALTER TABLE students DROP COLUMN date_of_birth;
 
-drop database geekbrains;
+-- Удаление таблиц
+DROP TABLE students;
+DROP TABLE courses;
+
+-- Удаляем базу данных
+DROP DATABASE geekbrains;
 
 
--- DML - Data Manupulation language
+-- команды DML (data manipulation language)
 -- CRUD
-insert into courses (name) values
-("Databases"),
+-- вставка данных
+INSERT INTO courses (name) VALUES
+("Databases"), 
 ("Linux"),
 ("BigDATA");
 
-insert into students (firstname, lastname, email, course_id) values
+INSERT INTO students (firstname, lastname, email, couse_id) VALUES 
 ("Pavel", "Durov", "ivanov@yandex.ru", "1"),
 ("Bill", "Gates", "petrov@yandex.ru", "2"),
 ("Ivan", "Grozny", "i.pushkina@yandex.ru", "3"),
-("Olga", "Buzova", "megastar@yandex.ru", "1");
+("Olga", "Buzova", "megastar@yandex.ru", "1")
 
-select * from courses;
-select * from students
+-- выборка данных
+SELECT * from courses;
+SELECT * from students
 where lastname = 'Gates';
 
+-- обновление данных
 update students
 set email = 'o.b@gmail.com'
-where lastname = 'Buzova'
+where lastname = 'Buzova';
 
+-- удаление данных
 delete from students
-where course_id = 1;
+where couse_id = 1;
 
-delete from students
+delete from students;
 
